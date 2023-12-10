e9b78208d8dff21d93d078a901a56299ecf42fff
trandung2k1 <trandungitksnb2001@gmail.com>
2023-12-10 16:37:36 +0700

    update code

--- src/app/comments/[id]/page.tsx
+++ src/app/comments/[id]/page.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 export async function generateStaticParams() {
-  // const rs = await fetch('http://localhost:3000/api/comments');
-  // const result = await rs.json();
-  // return result.map((i: any) => ({
-  //   id: `${i.id}`,
-  // }));
-  return [{ id: '1' }, { id: '2' }, { id: '3' }];
+  const rs = await fetch(`${process.env.BASE_URL}/api/comments`);
+  const result = await rs.json();
+  return result.map((i: any) => ({
+    id: `${i.id}`,
+  }));
+  // return [{ id: '1' }, { id: '2' }, { id: '3' }];
 }
 
 const Page = async ({ params }: { params: { id: string } }) => {
   const { id } = params;
   // getStaticProps with the revalidate
-  // const revalidatedData = await fetch(``, {
-  //   next: { revalidate: 10 },
-  // });
+  const revalidatedData = await fetch(`${process.env.BASE_URL}/api/comments/${id}`, {
+    next: { revalidate: 10 },
+  });
 
   // getStaticProps
-  // const staticData = await fetch(`https://...`, { cache: 'force-cache' });
+  // const staticData = await fetch(`${process.env.BASE_URL}/api/comments/${id}`, {
+  //   cache: 'force-cache',
+  // });
+
+  const data = await revalidatedData.json();
+  console.log(data);
   return <div>Page</div>;
 };
 
