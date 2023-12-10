1ff779130f258c14cee4c391fcf87f9e7d06d95c
trandung2k1 <trandungitksnb2001@gmail.com>
2023-12-10 16:07:45 +0700

    update

--- src/app/comments/[id]/page.tsx
+++ src/app/comments/[id]/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 export async function generateStaticParams() {
-  const rs = await fetch('http://localhost:3000/api/comments');
-  const result = await rs.json();
-  return result.map((i: any) => ({
-    id: `${i.id}`,
-  }));
-  // return [{ id: '1' }, { id: '2' }, { id: '3' }];
+  // const rs = await fetch('http://localhost:3000/api/comments');
+  // const result = await rs.json();
+  // return result.map((i: any) => ({
+  //   id: `${i.id}`,
+  // }));
+  return [{ id: '1' }, { id: '2' }, { id: '3' }];
 }
 
 const Page = async ({ params }: { params: { id: string } }) => {
